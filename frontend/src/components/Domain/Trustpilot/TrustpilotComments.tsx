62987be3edf07a4aa8e3ed8fe77155ecb61fba2c
Olexander <saschatimail@gmail.com>
2023-10-18 12:09:48 +0300

    feet: Added slider for component TrustpilotComments

--- frontend/src/components/Domain/Trustpilot/TrustpilotComments.tsx
+++ frontend/src/components/Domain/Trustpilot/TrustpilotComments.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from 'react'
+import React, { CSSProperties, memo, useCallback, useEffect, useMemo, useState } from 'react'
 import styles from './TrustpilotComments.module.scss'
 import classNames from 'classnames/bind'
 import Text from '@/UI/Text'
@@ -16,19 +16,70 @@ export type TrustpilotCommentsProps = {
   comments: CommentType[]
 }
 
+const SLIDER_SPEED = 2000
+const ITEM_WIDTH = 364
+
 const TrustpilotComments = memo<TrustpilotCommentsProps>(({ className, title, comments }): JSX.Element => {
+  const [slider, setSlider] = useState(() => ({
+    currIndex: 0,
+    trackWidth: 0,
+    width: 0,
+  }))
+
+  useEffect(() => {
+    let timer = setTimeout(function nextSlide() {
+      setSlider((prevSlider) => {
+        const currIndex = (prevSlider.currIndex + 1) % comments.length
+
+        return { ...prevSlider, currIndex }
+      })
+
+      if (timer) {
+        clearTimeout(timer)
+      }
+
+      timer = setTimeout(nextSlide, SLIDER_SPEED)
+    }, SLIDER_SPEED)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [comments.length])
+
   const commentsProps = useMemo(() => {
-    return comments.map(({ id, ...comment }) => (
-      <Comment key={id} className={cx('trustpilotComments__comment')} {...comment} />
-    ))
-  }, [comments])
+    return comments.map(({ id, ...comment }, index) => {
+      let active = slider.currIndex === index ? 'trustpilotComments__comment--active' : null
+      if (slider.width < slider.trackWidth) {
+        active = 'trustpilotComments__comment--active'
+      }
+
+      return <Comment key={id} className={cx('trustpilotComments__comment', active)} {...comment} />
+    })
+  }, [comments, slider])
+
+  const handleWidth = useCallback(
+    (e: HTMLDivElement | null) => e && setSlider((prevSlider) => ({ ...prevSlider, width: e.offsetWidth })),
+    [],
+  )
+  const handleTrackWidth = useCallback(
+    (e: HTMLDivElement | null) => e && setSlider((prevSlider) => ({ ...prevSlider, trackWidth: e.offsetWidth })),
+    [],
+  )
+
+  const commentsStyle: CSSProperties = {}
+
+  if (slider.width > slider.trackWidth) {
+    commentsStyle.transform = `translate(-${slider.currIndex * ITEM_WIDTH}px, 0)`
+  }
 
   return (
-    <div className={cx('trustpilotComments', className)}>
+    <div ref={handleTrackWidth} className={cx('trustpilotComments', className)}>
       <Text className={cx('trustpilotComments__title')} size="medium">
         {title}
       </Text>
-      <div className={cx('trustpilotComments__comments')}>{commentsProps}</div>
+      <div ref={handleWidth} className={cx('trustpilotComments__comments')} style={commentsStyle}>
+        {commentsProps}
+      </div>
     </div>
   )
 })
