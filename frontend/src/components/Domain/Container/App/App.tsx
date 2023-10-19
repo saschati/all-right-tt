c5342b4818e41d55b1a10a1675b08745cf3bacf5
Olexander <saschatimail@gmail.com>
2023-10-19 17:58:39 +0300

    fix: Change to BrowserRouter from HashRouter

--- frontend/src/components/Domain/Container/App/App.tsx
+++ frontend/src/components/Domain/Container/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from '@/app/store/redux/store'
 import env from '@/config/env'
@@ -7,7 +7,7 @@ import env from '@/config/env'
 const App: React.FC<React.PropsWithChildren> = ({ children }): JSX.Element => {
   return (
     <Provider store={store}>
-      <HashRouter basename={env.routeBaseName}>{children}</HashRouter>
+      <BrowserRouter basename={env.routeBaseName}>{children}</BrowserRouter>
     </Provider>
   )
 }
